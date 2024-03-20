e519a7a27db16c26a54dca70e7ff5573f5e0995f
Maryângela Soares <143448975+maryangelasoares@users.noreply.github.com>
2024-03-19 21:23:55 -0300

    Delete LÓGICA DE PROGRAMAÇÃO II directory

--- LÓGICA DE PROGRAMAÇÃO II/Exercícios (24.O1.24)/Atividade1.js
+++
@@ -1,9 +0,0 @@
-/* Escreva um programa que declare duas variáveis, “nome” e  “idade”, e as imprima em um console em uma frase que diga “Olá, 
-meu nome é [nome] e eu  tenho [idade] anos”. */
-
-// Declaração de variáveis.
-let nome = 'Maryângela';
-let idade = '30 Anos';
-
-// Imprime a frase.
-console.log(`Olá, meu nome é ${nome} e tenho ${idade}.`);
