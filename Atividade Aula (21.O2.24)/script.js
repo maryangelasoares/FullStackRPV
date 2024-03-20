2987620f00b5f877f2ece117a54189ac60a75f25
Maryângela Soares <143448975+maryangelasoares@users.noreply.github.com>
2024-03-19 21:31:12 -0300

    Delete Atividade Aula (21.O2.24) directory

--- Atividade Aula (21.O2.24)/script.js
+++
@@ -1,8 +0,0 @@
-const dropdownToggle = document.querySelector(".dropdown-toggle");
-const dropdownMenu = document.querySelector(".dropdown-menu");
-
-dropdownToggle.addEventListener("click", () => {
-  dropdownMenu.classList.toggle("active");
-  dropdownToggle.setAttribute("aria-expanded", dropdownMenu.classList.contains("active"));
-});
-
